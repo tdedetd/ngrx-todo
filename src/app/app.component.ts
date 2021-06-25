cb87f7c3c1ce46e5a8bb84e94a23c1ea26280528
tdedetd <tdedetd@gmail.com>
2021-06-26 00:06:25 +0300

    todo tag prefix; remove app component content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'todo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
